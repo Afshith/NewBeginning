7c216da4a79b3aae97139cff7f46a0f6a11b7614
Afshith <afshithafi@gmail.com>
2024-02-04 19:59:05 +0530

    4/2/2024

---
+++ HundredDaysOfCode/Day_27_RemoveElement.java
@@ -0,0 +1,16 @@
+package HundredDaysOfCode;
+
+public class Day_27_RemoveDuplicates {
+    public static void main(String[] args) {
+        int[] nums = {1,2,2,2,3,4,5};
+        int count = 0;
+        int value = 2;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i]!=value){
+                nums[count++] = nums[i];
+            }
+
+        }
+        System.out.println(count);
+    }
+}
---
+++ HundredDaysOfCode/Day_28_RotateString.java
@@ -0,0 +1,2 @@
+package HundredDaysOfCode;public class Day_28_RotateString {
+}
