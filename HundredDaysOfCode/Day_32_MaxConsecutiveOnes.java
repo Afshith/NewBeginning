5ec74addd6a307379e95768e39f626bab280b2c7
Afshith <afshithafi@gmail.com>
2024-02-21 15:32:08 +0530

    Completed Day 32 code MaxConsecutiveOnes

---
+++ HundredDaysOfCode/Day_32_MaxConsecutiveOnes.java
@@ -0,0 +1,22 @@
+package HundredDaysOfCode;
+
+public class Day_32_MaxConsecutiveOnes {
+    public static void main(String args[]) {
+        int nums[] = { 1, 1, 0, 1, 1, 1 };
+        int ans = findResult(nums);
+        System.out.println("The maximum  consecutive 1's are " + ans);
+    }
+    static int findResult(int[] nums){
+        int count = 0,max = 0;
+        for(int num :nums){
+            if(num == 1){
+                count++;
+            }
+            else {
+                count = 0;
+            }
+            max = Math.max(count ,max);
+        }
+        return max;
+    }
+}
